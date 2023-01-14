e94ba1881ec68dda8eebd2251d0eb8621ddfa5c7
Kamil Banachowicz <kamil.banachowicz.web@gmail.com>
2023-01-14 19:38:49 +0100

    Begining the new projects

---
+++ calculator.py
@@ -0,0 +1,6 @@
+
+
+def calculator():
+    a=5
+    b=2
+    print(eval("a+7+3-b"))
---
+++ main.py
@@ -0,0 +1,11 @@
+import calculator as calc
+
+print("Welcome in the set of applications.\nThis program contains some mini programs ready to use.")
+
+print("Available programs:\n1. Password generator.\n2. Hangman.\n3. Caesar cipher.\n4. Calculator.\n5. ... ")
+
+calc.calculator()
+
+
+
+
